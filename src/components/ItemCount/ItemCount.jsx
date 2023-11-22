c46a3ac32cdbe09d49fa78eca457055a5c79ef7c
Belen Barros <mbelen.barros@gmail.com>
2023-11-22 13:59:23 -0300

    Added ItemCount component

---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,37 @@
+import './ItemCount.css';
+import { useState } from 'react';
+
+const ItemCount = ({stock, initial, onAdd}) => {
+    const [quantity, setQuantity] = useState(initial)
+
+    const increment = () => {
+        if(quantity < stock) {
+            setQuantity(quantity + 1)
+        }
+    }
+
+
+    const decrement = () => {
+        if(quantity > 1) {
+            setQuantity(quantity - 1)
+        }
+    }
+
+    return(
+        <div className='counter'>
+            <div className='counter__controls'>
+                <button onClick={decrement}>-</button>
+                <h4>{quantity}</h4>
+                <button onClick={increment}>+</button>
+            </div>
+            <div className='button__cart'>
+                <button onClick={() => onAdd(quantity)} disabled={!stock}>
+                    Agregar al carrito
+                </button>
+            </div>
+        </div>
+    )
+
+}
+
+export default ItemCount;
